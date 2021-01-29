c6d7d2eec672bfe19c9df0bf3a35100e2939f757
Anne-Sophie Denommé-Pichon <anne-sophie.denomme-pichon@u-bourgogne.fr>
2021-01-29 23:05:23 +0100

    Remove loop in the wrapper

---
+++ wrapper_downsampling.sh
@@ -0,0 +1,21 @@
+#! /bin/sh
+
+DIJEN="dijen017"
+INPUT="/work/gad/shared/analyse/STR/Data/${DIJEN}/${DIJEN}/${DIJEN}.bam"
+DSDIR="/work/gad/shared/analyse/STR/Data/downsampling"
+DSR=10
+
+#DSR : downsampling rate (0 - 100)
+#DSRDIR : downsampled BAM directory
+
+set -x
+
+mkdir -p "${DSDIR}/${DIJEN}_downsampling_${DSR}_${i}"
+
+/user1/gad/an1770de/Scripts/bam_downsampling.py \
+   -i "${INPUT}" \
+   -o "${DSDIR}/${DIJEN}_downsampling_${DSR}_${i}/${DIJEN}_downsampling_${DSR}_${i}.bam" \
+   -d "${DSR}" \
+   -l "${DSDIR}/${DIJEN}_downsampling_${DSR}_${i}/${DIJEN}_downsampling_${DSR}_${i}.log"
+
+samtools index "${DSDIR}/${DIJEN}_downsampling_${DSR}_${i}/${DIJEN}_downsampling_${DSR}_${i}.bam"
