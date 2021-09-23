16a42095bdb5376f48be69846799205351be4936
Anne-Sophie Denommé-Pichon <anne-sophie.denomme-pichon@u-bourgogne.fr>
2021-09-24 00:15:46 +0200

    Add controls

--- launch_downsampling.sh
+++ launch_downsampling.sh
@@ -14,11 +14,20 @@ EXPERIMENT_COUNT=20
 COMPUTE_QUEUE="batch"
 
 for inputfile in \
-    "/work/gad/shared/analyse/STR/Data/dijen017/dijen017/dijen017.bam" \
-    "/work/gad/shared/analyse/STR/Data/dijen402/dijen402.bam"
+    "/work/gad/shared/analyse/STR/Data/dijen1013/dijen1013.bam"
+
+#    "/work/gad/shared/analyse/STR/Data/dijen081/dijen081.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen082/dijen082.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen083/dijen083.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen006/dijen006.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen1014/dijen1014.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen1015/dijen1015.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen017/dijen017/dijen017.bam" \
+#    "/work/gad/shared/analyse/STR/Data/dijen402/dijen402.bam"
+
 do
     sample="$(basename -s .bam "$inputfile")"
-    for rate in 5 10 15 20 25 30 35 40 # Downsampling rate: 0 - 100
+    for rate in 5 10 15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 #Downsampling rate: 0 - 100
     do
 	for experiment_number in $(seq $EXPERIMENT_COUNT)
 	do
