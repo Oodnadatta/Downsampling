3286a901aeb6b0dff51b4dac91d60e13b51d6116
Anne-Sophie Denommé-Pichon <anne-sophie.denomme-pichon@u-bourgogne.fr>
2021-09-24 00:27:49 +0200

    Clean file

--- bam_downsampling.py
+++ bam_downsampling.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# Yannis Duffourd
+# Anne-Sophie Denommé-Pichon
+
 import sys
 import getopt
 import pysam 
@@ -7,14 +10,12 @@ import logging
 import math
 import threading
 import random 
-# ~ import matplotlib.pyplot as plt
 
 currentThread = 0
 nbThread = 1
 downSamplingRate = 0
 inputFile = ""
 logFile = ""
-
 
 opts, args = getopt.getopt(sys.argv[1:], 'i:o:d:l:')  
 for opt, arg in opts:
@@ -33,13 +34,12 @@ for opt, arg in opts:
 sys.stderr.write( "Parsing bam file : %s ..." % ( inputFile ) )
 bamIterRef = pysam.AlignmentFile( inputFile , "r" )
 bamCodonReference = {}
-# samStream = open( samFile , "r" )
 bamOutStream = pysam.AlignmentFile(outputFile, "wb" , template=bamIterRef )
 for line in bamIterRef:
-	#logging.info('########\nNew read to parse : ' + str(line) )
+	# logging.info('########\nNew read to parse : ' + str(line) )
 	# pass bad alignements
 	if (line.is_unmapped == True ) or (line.is_secondary == True ) or (line.is_supplementary == True) :
-		#~ logging.info('Passing sequence : bad quality ' )
+		# logging.info('Passing sequence : bad quality ' )
 		continue
 	
 	
